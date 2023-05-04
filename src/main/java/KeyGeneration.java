b517ae5cb11c8cc5ecf2ee93eca7f5d3df2bd52c
Kyle <84309905+Bogandemon@users.noreply.github.com>
2023-05-04 17:29:49 +1000

    Compiled basic Key generation - Made basic key - Made skeleton Key generation file

---
+++ src/main/java/KeyGeneration.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class KeyGeneration {
+
+    private List<String> keyString;
+
+    public KeyGeneration(String fileName) {
+        try {
+            keyString = Utility.readAllLines(fileName);
+        }
+
+        catch (Exception e) {
+            System.err.println("Error Occurred: " + e);
+        }
+    }
+
+    //TO-DO
+    public List<String> leftSide() {
+        return keyString;
+    }
+
+    //TO-DO
+    public List<String> rightSide() {
+        return keyString;
+    }
+
+    //TO-DO
+    private List<String> keySeparation() {
+        return keyString;
+    }
+
+    //TO-DO
+    public List<String> shiftKey() {
+        return keyString;
+    }
+
+
+}

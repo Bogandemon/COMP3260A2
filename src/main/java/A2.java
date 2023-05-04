cbd26356b11fd360207a8a61a1342efa6fd72708
Kyle <84309905+Bogandemon@users.noreply.github.com>
2023-05-04 16:18:16 +1000

    Can read files from resources - Added basic utility class - Added basic information text file - Added empty key and small plaintext file - Reads input from plaintext file

--- src/main/java/A2.java
+++ src/main/java/A2.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class A2 {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        List<String> plaintext = new ArrayList<>();
+
+        try {
+            plaintext = Utility.readAllLines("src/resources/Plaintext.txt");
+        }
+
+        catch (Exception e) {
+            System.err.println("Error Occurred: " + e);
+        }
+
+        for (int i=0; i<plaintext.size(); i++) {
+            System.out.println(plaintext.get(i));
+        }
     }
 }

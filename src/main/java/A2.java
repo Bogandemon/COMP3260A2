a7d41956f07cd5468ffaf894b21f7d88a724e0d8
JoshObrien10 <joshfp.obrien@gmail.com>
2023-05-15 23:24:26 +1000

    sbox method complete

--- src/main/java/A2.java
+++ src/main/java/A2.java
@@ -3,65 +3,50 @@ import java.util.List;
 import java.util.Random;
 import javax.crypto.KeyGenerator;
 
-public class A2 {
+public class A2
+{
     public static void main(String[] args)
     {
-                int[][][] sBoxes = new int[8][4][16];
-                Random random = new Random();
-
-                // Fill the S-boxes with random values
-                for (int s = 0; s < 8; s++) {
-                    System.out.print( "{ ");
-                    for (int row = 0; row < 4; row++) {
-                        System.out.print( "{ ");
-                        for (int col = 0; col < 16; col++)
-                        {
-                                int randoem = random.nextInt(16);
-                            while( notThere( sBoxes[s][row], randoem )   )
-                            {
-                                sBoxes[s][row][col] = random.nextInt(16);
-                                System.out.print( sBoxes[s][row][col] + ", ");
-                                randoem =  random.nextInt(16);
-                            }
-                        }
-                        System.out.println( "}, ");
-                    }
-                    System.out.println( "}, ");
-                }
-
-        System.out.println(sBoxes);
         List<String> plaintext = new ArrayList<>();
-
-        try {
+        List<List<List<Integer>>> sBoxes = generateSBoxes();
+        try
+        {
             plaintext = Utility.readAllLines("src/resources/Plaintext.txt");
         }
 
-        catch (Exception e) {
+        catch (Exception e)
+        {
             System.err.println("Error Occurred: " + e);
         }
 
-        for (int i=0; i<plaintext.size(); i++) {
+        for (int i=0; i<plaintext.size(); i++)
+        {
             System.out.println(plaintext.get(i));
         }
-
-
     }
 
-    private static boolean notThere(int[] sBox, int provided)
+    private static List<List<List<Integer>>> generateSBoxes()
     {
-
-        boolean amount=false;
-        for(int s: sBox)
+        List<List<List<Integer>>> sBoxes = new ArrayList<>();
+        Random random = new Random();
+        for(int s = 0; s < 8; s++)
         {
-            if(s == provided )
+            List<List<Integer>> sbox = new ArrayList<>();
+            for( int row = 0; row < 4; row++ )
             {
-               amount = true;
+                List<Integer> rowInt = new ArrayList<>();
+                while( rowInt.size()<16 )
+                {
+                    int randomInt = random.nextInt( 16 );
+                    if( !rowInt.contains( randomInt ) )
+                    {
+                        rowInt.add( randomInt );
+                    }
+                }
+                sbox.add( rowInt );
             }
+            sBoxes.add( sbox );
         }
-        if(  sBox.length>15)
-        {
-            amount=false;
-        }
-        return amount;
+        return sBoxes;
     }
 }

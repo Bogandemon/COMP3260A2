296520ed94fc62f5dc54300a0ed8a1736f8e0d51
JoshObrien10 <joshfp.obrien@gmail.com>
2023-05-19 00:43:30 +1000

    des encryptions and avalache report complete

--- src/main/java/A2.java
+++ src/main/java/A2.java
@@ -1,52 +1,127 @@
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import javax.crypto.KeyGenerator;
 
 public class A2
 {
     public static void main(String[] args)
     {
-        List<String> plaintext = new ArrayList<>();
-        List<List<List<Integer>>> sBoxes = generateSBoxes();
+        List<String> input = new ArrayList<>();
         try
         {
-            plaintext = Utility.readAllLines("src/resources/Plaintext.txt");
+           input = Files.readAllLines(Path.of("src/resources/Plaintext.txt"));
         }
-
         catch (Exception e)
         {
-            System.err.println("Error Occurred: " + e);
+           e.printStackTrace();
         }
+        
+        List<List<Integer>> avalancheResults = new ArrayList<>();
+        DES0 des0P = new DES0(input.get(0), input.get(2));
+        DES0 des0Q = new DES0(input.get(1), input.get(2));
+        des0P.encrypt();
+        des0Q.encrypt();
+        avalancheResults.add(avalanche(des0P.roundOutputs, des0Q.roundOutputs));
 
-        for (int i=0; i<plaintext.size(); i++)
+        DES1 des1P = new DES1(input.get(0), input.get(2));
+        DES1 des1Q = new DES1(input.get(1), input.get(2));
+        des1P.encrypt();
+        des1Q.encrypt();
+        avalancheResults.add(avalanche(des1P.roundOutputs, des1Q.roundOutputs));
+
+        DES2 des2P = new DES2(input.get(0), input.get(2));
+        DES2 des2Q = new DES2(input.get(1), input.get(2));
+        des2P.encrypt();
+        des2Q.encrypt();
+        avalancheResults.add(avalanche(des2P.roundOutputs, des2Q.roundOutputs));
+
+        DES3 des3P = new DES3(input.get(0), input.get(2));
+        DES3 des3Q = new DES3(input.get(1), input.get(2));
+        des3P.encrypt();
+        des3Q.encrypt();
+        avalancheResults.add(avalanche(des3P.roundOutputs, des3Q.roundOutputs));
+        
+        List<List<Integer>> avalancheResults2 = new ArrayList<>();
+        DES0 des0K = new DES0(input.get(0), input.get(2));
+        DES0 des0L = new DES0(input.get(0), input.get(3));
+        des0K.encrypt();
+        des0L.encrypt();
+        avalancheResults2.add(avalanche(des0K.roundOutputs, des0L.roundOutputs));
+
+        DES1 des1K = new DES1(input.get(0), input.get(2));
+        DES1 des1L = new DES1(input.get(0), input.get(3));
+        des1K.encrypt();
+        des1L.encrypt();
+        avalancheResults2.add(avalanche(des1K.roundOutputs, des1L.roundOutputs));
+
+        DES2 des2K = new DES2(input.get(0), input.get(2));
+        DES2 des2L = new DES2(input.get(0), input.get(3));
+        des2K.encrypt();
+        des2L.encrypt();
+        avalancheResults2.add(avalanche(des2K.roundOutputs, des2L.roundOutputs));
+
+        DES3 des3K = new DES3(input.get(0), input.get(2));
+        DES3 des3L = new DES3(input.get(0), input.get(3));
+        des3K.encrypt();
+        des3L.encrypt();
+        avalancheResults2.add(avalanche(des3K.roundOutputs, des3L.roundOutputs));
+
+        try( PrintWriter writer = new PrintWriter("avalancheReport.txt") )
         {
-            System.out.println(plaintext.get(i));
+            writer.println( "Avalanche Demonstration" );
+            writer.println( "Plaintext P: " + input.get(0) );
+            writer.println( "Plaintext P’: " + input.get(1) );
+            writer.println( "Key K: " + input.get(2) );
+            writer.println( "Key K’: " + input.get(2) );
+            writer.println( "Total running time: " + input.get(2) + "(second)" );
+
+            writer.println("\nP and P’ under K");
+            writer.println( "Ciphertext C: " + des0P.roundOutputs.get(16) );
+            writer.println( "Ciphertext C’: " + des0Q.roundOutputs.get(16) );
+            outPutAvalanche( avalancheResults, writer );
+
+            writer.println("\nP under K and K’");
+            writer.println( "Ciphertext C: " + des0K.roundOutputs.get(16) );
+            writer.println( "Ciphertext C’: " + des0L.roundOutputs.get(16) );
+            outPutAvalanche( avalancheResults2, writer );
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException(e);
         }
     }
 
-    private static List<List<List<Integer>>> generateSBoxes()
+
+    private static List<Integer> avalanche(List<String> roundOutputs, List<String> roundOutputs1)
     {
-        List<List<List<Integer>>> sBoxes = new ArrayList<>();
-        Random random = new Random();
-        for(int s = 0; s < 8; s++)
+        List<Integer> results = new ArrayList<>();
+        for( int i=0; i<roundOutputs.size(); i++)
         {
-            List<List<Integer>> sbox = new ArrayList<>();
-            for( int row = 0; row < 4; row++ )
+            int count=0;
+            for(int j=0; j<64; j++)
             {
-                List<Integer> rowInt = new ArrayList<>();
-                while( rowInt.size()<16 )
+                if( roundOutputs.get(i).charAt(j) != roundOutputs1.get(i).charAt(j))
                 {
-                    int randomInt = random.nextInt( 16 );
-                    if( !rowInt.contains( randomInt ) )
-                    {
-                        rowInt.add( randomInt );
-                    }
+                    count++;
                 }
-                sbox.add( rowInt );
             }
-            sBoxes.add( sbox );
+            results.add(count);
         }
-        return sBoxes;
+        return results;
+    }
+
+    private static void outPutAvalanche( List<List<Integer>> avalancheResults, PrintWriter writer )
+    {
+        for(int i=0; i<17; i++)
+        {
+            writer.print(i+" ");
+            for(int j=0; j<4; j++)
+            {
+                writer.print(avalancheResults.get(j).get(i) +" ");
+            }
+            writer.print("\n");
+        }
     }
 }
--- src/main/java/KeyGeneration.java
+++ src/main/java/KeyGeneration.java
@@ -1,132 +1,38 @@
-/*
- * Classname: KenGeneration
- * Programmer: Kyle Dryden
- * Version: Java 17
- * Date: 10/05/2023
- * Description: KeyGeneration class that handles any key operations, such as shifting, separation, and permutations.
- */
-
 import java.util.List;
 
 public class KeyGeneration {
 
-    private String keyString; //String variable that holds the key obtained from the text file and permutation from permutated choice 2.
-    private String leftSide; //String variable that holds the left (C) portion of the key during rounds.
-    private String rightSide; //String variable that holds the right (D) portion of the key during rounds.
-    private int[][] permutationChoice1; //2D list variable used for the initial permutation used on the key.
-    private int[][] permutationChoice2; //2D list variable used for all permutations performed during rounds for the key.
+    private List<String> keyString;
 
     public KeyGeneration(String fileName) {
-        keyString = "";
-        leftSide = "";
-        rightSide = "";
-
         try {
-            List<String> convertString = Utility.readAllLines(fileName);
-
-            for (String s : convertString) {
-                keyString += s;
-            }
+            //keyString = Utility.readAllLines(fileName);
         }
 
         catch (Exception e) {
             System.err.println("Error Occurred: " + e);
         }
-
-        init();
     }
 
-    private void init() {
-        permutationChoice1 = new int[][]{{57, 49, 41, 33, 25, 17, 9},
-                                        {1, 58, 50, 42, 34, 26, 18},
-                                        {10, 2, 59, 51, 43, 35, 27},
-                                        {19, 11, 3, 60, 52, 44, 36},
-                                        {63, 55, 47, 39, 31, 23, 15},
-                                        {7, 62, 54, 46, 38, 30, 22},
-                                        {14, 6, 61, 53, 45, 37, 29},
-                                        {21, 13, 5, 28, 20, 12, 4}};
-
-        permutationChoice2 = new int[][]{{14, 17, 11, 24, 1, 5, 3, 28},
-                                        {15, 6, 21, 10, 23, 19, 12, 4},
-                                        {26, 8, 16, 7, 27, 20, 13, 2},
-                                        {41, 52, 31, 37, 47, 55, 30, 40},
-                                        {51, 45, 33, 48, 44, 49, 39, 56},
-                                        {34, 53, 46, 42, 50, 36, 29, 32}};
-
-        keySeparation();
-    }
-
-    private void keySeparation() {
-        int keyCharacter = 0;
-        for (int i=0; i<permutationChoice1.length/2; i++) {
-            for (int m=0; m<permutationChoice1.length-1; m++) {
-                keyCharacter = permutationChoice1[i][m];
-
-                leftSide += keyString.substring(keyCharacter-1, keyCharacter);
-            }
-        }
-
-        for (int i=0; i<permutationChoice1.length/2; i++) {
-            for (int m=0; m<permutationChoice1.length-1; m++) {
-                keyCharacter = permutationChoice1[i+(permutationChoice1.length/2)][m];
-
-                rightSide += keyString.substring(keyCharacter-1, keyCharacter);
-            }
-        }
-    }
-
-    public void roundPermute() {
-        String outputString = "";
-        String tempCombine = leftSide + rightSide;
-        int keyCharacter = 0;
-
-        keyString = "";
-
-        for (int i=0; i< permutationChoice2.length; i++) {
-            for (int m = 0; m<8; m++) {
-                keyCharacter = permutationChoice2[i][m];
-
-                keyString += tempCombine.substring(keyCharacter - 1, keyCharacter);
-            }
-        }
-    }
-
-    public void shiftKey(int shiftAmount, boolean leftCheck) {
-        String outputString = "";
-        String loopString = "";
-
-        if (leftCheck) {
-            loopString = leftSide;
-        }
-
-        else {
-            loopString = rightSide;
-        }
-
-        for (int i = 0; i < loopString.length(); i++) {
-            int index = (i + shiftAmount) % 28;
-
-            outputString += loopString.substring(index, index + 1);
-        }
-
-        if (leftCheck) {
-            leftSide = outputString;
-        }
-
-        else {
-            rightSide = outputString;
-        }
-    }
-
-    public String getKeyString() {
+    //TO-DO
+    public List<String> leftSide() {
         return keyString;
     }
 
-    public String getLeftSide() {
-        return leftSide;
+    //TO-DO
+    public List<String> rightSide() {
+        return keyString;
     }
 
-    public String getRightSide() {
-        return rightSide;
+    //TO-DO
+    private List<String> keySeparation() {
+        return keyString;
     }
+
+    //TO-DO
+    public List<String> shiftKey() {
+        return keyString;
+    }
+
+
 }
